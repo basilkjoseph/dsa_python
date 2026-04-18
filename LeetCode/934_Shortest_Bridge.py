1c95845a1347d1d2cd0dc20c0865a1e306b549b8
Basil Joseph <basilkjoseph@users.noreply.github.com>
2026-04-18 15:58:26 +0530

    Optimized LeetCode 934_Shortest_Bridge

--- LeetCode/934_Shortest_Bridge.py
+++ LeetCode/934_Shortest_Bridge.py
@@ -72,6 +72,48 @@ class Solution:
         return min_bridge
 
             
-        
-                
+#Approach 2 (Optimized)
+class Solution:
+    def connectIslands(self,grid,idx,island):
+        directions=((0,1),(0,-1),(1,0),(-1,0))
+        bfs=deque()
+        bfs.append(idx)
+        island.add(idx)
+        while(bfs):
+            (x,y)=bfs.popleft()
+            for dx,dy in directions:
+                new_x,new_y=x+dx,y+dy
+                if 0<=new_x<len(grid) and 0<=new_y<len(grid[0]) and (new_x,new_y) not in island:
+                    if grid[new_x][new_y]==1:
+                        island.add((new_x,new_y))
+                        bfs.append((new_x,new_y))
+
+    def bridgeIslands(self,grid,island):
+        directions=((0,1),(0,-1),(1,0),(-1,0))
+        bfs=deque((cell,0) for cell in island)
+        visited=set(island)
+        while(bfs):
+            (x,y),count=bfs.popleft()
+            for dx,dy in directions:
+                new_x,new_y=x+dx,y+dy
+                if 0<=new_x<len(grid) and 0<=new_y<len(grid[0]) and (new_x,new_y) not in visited:
+                    if grid[new_x][new_y]==1:
+                        return count
+                    visited.add((new_x,new_y))
+                    bfs.append(((new_x,new_y),count+1))
+        return -1
+
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        island=set()
+        flag=False
+        #Connecting island
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==1:
+                    self.connectIslands(grid,(i,j),island)
+                    flag=True
+                    break
+            if flag:
+                break
+        return self.bridgeIslands(grid,island)
         
